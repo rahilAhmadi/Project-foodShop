dbf89947ef126f8513ac65ef83c53903a5ba7113
habiballahi-zahra <149077443+habiballahi-zahra@users.noreply.github.com>
2025-11-03 09:21:17 +0330

    Add fonts folder to assets

--- website/register.php
+++ website/register.php
@@ -9,7 +9,7 @@ if(isset($_POST['input_name']) && !empty($_POST['input_name'])&&
     $user_Name = $_POST['input_user'];
     $user_Password = $_POST['input_pass'];
     $connect = mysqli_connect('localhost', 'root', '', 'daspokht');
-    $sql = 'insert into users (role_ID,full_Name,user_Name,user_Password) VALUES (1,"'.$full_Name.'","'.$user_Name.'","'.$user_Password.'")';
+    $sql = 'insert into users (role_ID,full_Name,user_Name,user_Password) VALUES (2,"'.$full_Name.'","'.$user_Name.'","'.$user_Password.'")';
     $query = mysqli_query($connect, $sql);
     echo $full_Name."++++".$user_Name."++++".$user_Password."true";
 }
