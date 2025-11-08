edc15a2fe2ba6592ba99e422b49595cb019e858e
habiballahi-zahra <149077443+habiballahi-zahra@users.noreply.github.com>
2025-11-08 10:33:17 +0330

    Add SessionCheck.php for check Last activity time

---
+++ website/SessionCheck.php
@@ -0,0 +1,27 @@
+<?php
+
+$timeout_seconds = 30;
+
+if (isset($_SESSION['last_activity'])) 
+    {
+    $inactive = time() - (int)$_SESSION['last_activity'];
+    if ($inactive > $timeout_seconds) 
+        {
+            session_unset();
+            session_destroy();
+            header("Location: login.html");
+        }
+    else 
+        {
+
+            $_SESSION['last_activity'] = time();
+        }
+    }
+
+else 
+    {
+    header("Location: login.php");
+    exit;
+    }
+
+?>
