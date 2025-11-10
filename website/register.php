7c4632e8321339ed6a1dcc8b4d272a888bc9ebe1
habiballahi-zahra <149077443+habiballahi-zahra@users.noreply.github.com>
2025-11-10 20:44:55 +0330

    Add rules ckeck Username uniqueness

--- website/register.php
+++ website/register.php
@@ -4,24 +4,44 @@ $user_Name ="user";
 $user_Password ="pass";
 if(isset($_POST['input_name']) && !empty($_POST['input_name'])&&
     isset($_POST['input_user'])&& !empty($_POST['input_user'])&&
-    isset($_POST['input_pass'])&& !empty($_POST['input_pass'])) {
+    isset($_POST['input_pass'])&& !empty($_POST['input_pass'])) 
+  {
     $full_Name = $_POST['input_name'];
     $user_Name = $_POST['input_user'];
     $user_Password = $_POST['input_pass'];
     $connect = mysqli_connect('localhost', 'root', '', 'daspokht');
-    $sql = 'insert into users (role_ID,full_Name,user_Name,user_Password) VALUES (2,"'.$full_Name.'","'.$user_Name.'","'.$user_Password.'")';
-    $query = mysqli_query($connect, $sql);
+    $check='SELECT user_Name FROM users WHERE user_Name ='.$user_Name;
+    $query = mysqli_query($connect, $check);
+    // var_dump($query) ;
+    if($query == false)
+    {
+       echo "<script>
+                alert('نام کاربری تکراری است');
+                window.location.href='register.html';
+              </script>";
+
+    }
+    else
+    {
+      $sql = 'insert into users (role_ID,full_Name,user_Name,user_Password) VALUES (2,"'.$full_Name.'","'.$user_Name.'","'.$user_Password.'")';
+       $query = mysqli_query($connect, $sql);
         echo "<script>
                 alert('ثبت‌نام موفق بود');
-                window.location.href='login.html';
+               window.location.href='login.html';
               </script>";
               echo $full_Name."++++".$user_Name."++++".$user_Password."true";
-    } else {
+     
+
+    }
+  }
+else 
+  {
         echo "<script>
                 alert('خطایی رخ داد');
                 window.location.href='register.html';
               </script>";
-    }
+  }
+    
 
 mysqli_close($connect);
 ?>
