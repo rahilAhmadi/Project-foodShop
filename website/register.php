aed5eab6701c843cc6c107aadfffac0e6f86362f
habiballahi-zahra <149077443+habiballahi-zahra@users.noreply.github.com>
2025-11-12 18:25:27 +0330

    Add  check_username_uniuqe function to register.php To resolve the duplicate user addition bug

--- website/register.php
+++ website/register.php
@@ -9,11 +9,10 @@ if(isset($_POST['input_name']) && !empty($_POST['input_name'])&&
     $full_Name = $_POST['input_name'];
     $user_Name = $_POST['input_user'];
     $user_Password = $_POST['input_pass'];
-    $connect = mysqli_connect('localhost', 'root', '', 'daspokht');
-    $check='SELECT user_Name FROM users WHERE user_Name ='.$user_Name;
-    $query = mysqli_query($connect, $check);
-    // var_dump($query) ;
-    if($query == false)
+
+    $check_Username=check_username_uniuqe($user_Name);
+   
+    if($check_Username)
     {
        echo "<script>
                 alert('نام کاربری تکراری است');
@@ -23,13 +22,12 @@ if(isset($_POST['input_name']) && !empty($_POST['input_name'])&&
     }
     else
     {
-      $sql = 'insert into users (role_ID,full_Name,user_Name,user_Password) VALUES (2,"'.$full_Name.'","'.$user_Name.'","'.$user_Password.'")';
-       $query = mysqli_query($connect, $sql);
+        add_user($full_Name,$user_Name,$user_Password);
         echo "<script>
                 alert('ثبت‌نام موفق بود');
                window.location.href='login.html';
               </script>";
-              echo $full_Name."++++".$user_Name."++++".$user_Password."true";
+              // echo $full_Name."++++".$user_Name."++++".$user_Password."true";
      
 
     }
@@ -44,4 +42,36 @@ else
     
 
 mysqli_close($connect);
+
+
+
+ 
+function check_username_uniuqe($user_Name)
+{
+  $countUsername=0;
+  $connect = mysqli_connect('localhost', 'root', '', 'daspokht');
+  $check="SELECT user_Name FROM users WHERE user_Name ='".$user_Name."'";
+  // echo $check;
+  $query = mysqli_query($connect, $check);
+  while($row=mysqli_fetch_array($query))
+  {
+    $countUsername +=1;
+  }
+  if($countUsername>0)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
+function add_user($full_Name,$user_Name,$user_Password)
+{
+  $connect = mysqli_connect('localhost', 'root', '', 'daspokht');
+  $sql = 'insert into users (role_ID,full_Name,user_Name,user_Password) VALUES (2,"'.$full_Name.'","'.$user_Name.'","'.$user_Password.'")';
+  $query = mysqli_query($connect, $sql);
+
+}
 ?>
