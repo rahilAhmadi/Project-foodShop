9c4a6d43a34f830ff69fbff5cb5cf2ff13daa582
habiballahi-zahra <149077443+habiballahi-zahra@users.noreply.github.com>
2025-11-14 14:03:09 +0330

    Add َAddFood function to insert food to database and upload food image to FoodsImage folder.

--- website/AddFood.php
+++ website/AddFood.php
@@ -1,24 +1,13 @@
 <?php
-$food_Name ="name";
-$food_Price =0;
-$description ="description";
+$food_info="";
 $image="image";
-$type="type";
-if( isset($_POST['food_name'])&& !empty($_POST['food_name'])&&
-    isset($_POST['food_description'])&& !empty($_POST['food_description'])&&
-    isset($_POST['food_price'])&& !empty($_POST['food_price'])&&
-    isset($_POST['food_type'])&& !empty($_POST['food_type'])&&
+if( isset($_POST['food_info'])&& !empty($_POST['food_info'])&&
     isset($_FILES['food_image'])&& !empty($_FILES['food_image'])) 
 {
-    $food_Name=$_POST['food_name'];
-    $food_Price=$_POST['food_price'];
-    $description=$_POST['food_description'];
-    $image=$_FILES['food_image'];
-    $type=$_POST['food_type'];
+    $food_info=$_POST['food_info'];
+    $image_name=Uploader();
+    AddFood($food_info,$image_name);
 
-    Uploader();
-
-    echo $food_Name."++".$food_Price."++".$description."++".$image['name']."++".$type;
 
 }
 else 
@@ -33,14 +22,25 @@ echo"<a href='panel.html#addProduct'>add</a>";
 
 function Uploader()
 {
-$pic_name=$_FILES['food_image']['name'];
-$array=explode(".",$pic_name);
-$ext=end($array);
-$new_name=rand().".".$ext;
-$from=$_FILES['food_image']['tmp_name'];
-$to="asset/img/FoodsImage/".$new_name;
-move_uploaded_file($from,$to) ;
+    $pic_name=$_FILES['food_image']['name'];
+    $array=explode(".",$pic_name);
+    $ext=end($array);
+    $new_name=rand().".".$ext;
+    $from=$_FILES['food_image']['tmp_name'];
+    $to="asset/img/FoodsImage/".$new_name;
+    move_uploaded_file($from,$to) ;
+    return $new_name;
 
+}
+
+function AddFood($food_info,$image_name)
+{
+
+    var_dump($food_info);
+    echo $image_name;
+    $connect = mysqli_connect('localhost', 'root', '', 'daspokht');
+    $sql = 'INSERT INTO foods (food_Name, description, price, img_url) VALUES ("'.$food_info['name'].'","'.$food_info['description'].'","'.$food_info['price'].'","'.$image_name.'")';
+    $query = mysqli_query($connect, $sql);
 }
 
 ?>
