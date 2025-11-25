5fb57707b841851499af0759c570031e5391f964
fatemehdadvand <fatemehdadvand81@gmail.com>
2025-11-25 17:02:56 +0330

    link to AddFood.js

--- website/Admin/panel.php
+++ website/Admin/panel.php
@@ -473,7 +473,7 @@ if (isset($_GET['Avalible']))
                         <span class="error-message" id="descriptionError"></span>
                     
                         <h3 class="label_food">قیمت:</h3>
-                        <input type="text" class="input_food"  name="food_info[price]"  placeholder="مثلاً 250000" id="price"  value="<?= $food ? $food['price'] : '' ?>">
+                        <input type="text" class="input_food"  name="food_info[price]"  placeholder="مثلا 50.000 تومان" id="price"  value="<?= $food ? $food['price'] : '' ?>">
                         <span class="error-message" id="priceError"></span>
                         <div class="food-select-box">
                             <label for="food-type" class="food-label">انتخاب نوع غذا</label>
@@ -574,6 +574,7 @@ if (isset($_GET['Avalible']))
         </main>
     </section>
     <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.0/dist/Chart.min.js"></script>
+    <script src="../js/AddFood.js"></script>
    <!-- <script src="js/main.js"></script> -->
    <script>
     let inputs = document.querySelectorAll("input[type=file]");
@@ -697,108 +698,6 @@ if (isset($_GET['Avalible']))
     
     
     
-// add food js
-// const form = document.querySelector(".form_food");
-// const imageInput = document.getElementById("image");
-// const imagePreview = document.getElementById("imagePreview");
-
-// imageInput.addEventListener("change", function() {
-//     const file = this.files[0];
-//     if (file) {
-//         const reader = new FileReader();
-//         reader.onload = function(e) {
-//             imagePreview.src = e.target.result;
-//             imagePreview.style.display = "block";
-//         }
-//         reader.readAsDataURL(file);
-//     } else {
-//         imagePreview.src = "";
-//         imagePreview.style.display = "none";
-//     }
-// });
-
-// form.addEventListener("submit", function (e) {
-//   e.preventDefault();
-
-//   const fields = [
-//     { input: document.getElementById("name"), error: document.getElementById("nameError"), type: "persian" },
-//     { input: document.getElementById("description"), error: document.getElementById("descriptionError"), type: "persian" },
-//     { input: document.getElementById("price"), error: document.getElementById("priceError"), type: "number" },
-//     { input: document.getElementById("image"), error: document.getElementById("imageError"), type: "image" }
-//   ];
-
-//   const persianRegex = /^[\u0600-\u06FF\s]+$/;
-//   const numberRegex = /^[0-9]+$/;
-//   const imageRegex = /\.(jpg|jpeg|png|gif|bmp)$/i;
-
-//   let valid = true;
-
-  
-//   fields.forEach(f => {
-//     f.error.textContent = "";
-//     f.input.classList.remove("input-error");
-//   });
-
-//   fields.forEach(f => {
-//     const value = f.input.value.trim();
-//     if (f.type === "persian") {
-//       if (!value) {
-//         f.error.textContent = "این فیلد را وارد کنید.";
-//         f.input.classList.add("input-error");
-//         valid = false;
-//       } else if (!persianRegex.test(value)) {
-//         f.error.textContent = "فقط حروف فارسی مجاز است.";
-//         f.input.classList.add("input-error");
-//         valid = false;
-//       }
-//     }
-
-//     if (f.type === "number") {
-//       if (!value) {
-//         f.error.textContent = "این فیلد را وارد کنید.";
-//         f.input.classList.add("input-error");
-//         valid = false;
-//       } else if (!numberRegex.test(value)) {
-//         f.error.textContent = "فقط عدد مجاز است.";
-//         f.input.classList.add("input-error");
-//         valid = false;
-//       } else {
-//         const priceNum = parseInt(value, 10);
-//         if (priceNum < 250000 || priceNum > 1000000) {
-//           f.error.textContent = "قیمت باید بین 250,000 تا 1,000,000 تومان باشد.";
-//           f.input.classList.add("input-error");
-//           valid = false;
-//         }
-//       }
-//     }
-
-//     if (f.type === "image") {
-//       const file = f.input.files[0];
-//       if (!file) {
-//         f.error.textContent = "یک تصویر انتخاب کنید.";
-//         f.input.classList.add("input-error");
-//         valid = false;
-//       } else if (!imageRegex.test(file.name)) {
-//         f.error.textContent = "فرمت تصویر معتبر نیست (jpg, png, gif, bmp).";
-//         f.input.classList.add("input-error");
-//         valid = false;
-//       }
-//     }
-//   });
-
-//   if (valid) {
-//     alert("غذای جدید با موفقیت به منو اضافه شد!");
-//       form.reset();
-
-//     // پاک کردن پیش‌نمایش تصویر
-//     imagePreview.src = "";
-//     imagePreview.style.display = "none";
-
-//     // پاک کردن کلاس‌های خطا
-//     fields.forEach(f => f.input.classList.remove("input-error"));
-//     // form.submit();
-//   }
-// });
 
 //    ++++++++++++++++++++++++ بخش باز کردن تب از صفحه php+++++++++++++
      // فعال‌سازی تب از طریق پارامتر URL
