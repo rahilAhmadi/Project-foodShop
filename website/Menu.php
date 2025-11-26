02366003218130170941922accb4b5f14bbc1e56
Faezeh Ehsanfar <fantasticp.17@gmail.com>
2025-11-26 06:38:14 +0330

    edit menu.php

--- website/Menu.php
+++ website/Menu.php
@@ -25,7 +25,7 @@ if (isset($_SESSION['login'])&& $_SESSION['login']===true)
 
 echo'       <div class="menuLeft">
                 <ul>
-                    <li><a href="shopping-card.html"><img src="asset/icon/card 1.png" alt="shoping-card"></a></li>
+                    <li><a href="shoppingCart.html"><img src="asset/icon/card 1.png" alt="shoping-card"></a></li>
                     <li>
                         <a href="logOut.php" class="buttonLogin btn_menu_a">'.$_SESSION['name'].'</a>
                         <a href="logOut.php" class="logoLogin btn_menu_a"><img src="asset/icon/user.png" alt="user"></a>
